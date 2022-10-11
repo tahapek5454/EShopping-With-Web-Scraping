85bb55f28160f04978bab98e10450bf90ea79ed6
Taha Pek <tahapek5454@gmail.com>
2022-10-11 21:31:36 +0300

    add navbar link and css for it

--- shoppingApp/shopping/urls.py
+++ shoppingApp/shopping/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 # urlpatterns icine uygulanamanın yonlenecegi url leri belirtiriz
 urlpatterns = [
-    path("", views.home),
-    path("<int:id>", views.productDetails), # detail da simdilik her pc ozgu id gelsin olarak dusunelim
+    path("", views.home, name='home'),
+    path("<int:id>", views.productDetails, name='productDetails'), # detail da simdilik her pc ozgu id gelsin olarak dusunelim
 ]
