3204968be546fb7aa42c0a41c6df1a36fc94b1dc
Taha Pek <tahapek5454@gmail.com>
2022-10-12 21:50:09 +0300

    add denemeProduct for test

--- shoppingApp/shopping/admin.py
+++ shoppingApp/shopping/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import DenemeProduct
 # Register your models here.
+# admin uygulamasında gostertiyoruz
+
+admin.site.register(DenemeProduct)
--- shoppingApp/shopping/models.py
+++ shoppingApp/shopping/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# Buraya Modellerimizi yüklüyoruz
+# Ben bir deneme modeli yükleyeceğim
+
+
+class DenemeProduct(models.Model):
+    # burası bizim tablomuzun oldugu yer
+    title = models.CharField(max_length = 200)
+    price = models.CharField(max_length = 100)
+    rating = models.CharField(max_length = 50)
+
+    
