b58c27df0d78c5aa7ab9dd274b663a1589412204
MelihSelamiUrkmez <melih.s.urkmez@gmail.com>
2022-10-19 22:45:16 +0300

    Pagination eklendi

--- EcommerceWeb/ecommerce/views.py
+++ EcommerceWeb/ecommerce/views.py
@@ -1,12 +1,27 @@
 import re
 from django.shortcuts import render
 from .models import Prods
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def homepage(request):
-    prod= {
-        "prods": Prods.objects.all()
-    } 
+    posts=Prods.objects.all()
+    page = request.GET.get('page')
+    num_of_item= 20
+    paginator= Paginator(posts,num_of_item)
+    
+    try:
+        posts=paginator.page(page)
+    except PageNotAnInteger:
+        page=1
+        posts=paginator.page(page)
+    except EmptyPage:
+        page=paginator.num_pages
+        posts=paginator.page(page)
+    
+    
+    
+    prod={'prods':posts,'paginator':paginator}
+    
     return render(request,'ecommerce/home.html',prod)
 
 def prodDetails(request,id):
@@ -16,25 +31,68 @@ def prodDetails(request,id):
     return render(request,'ecommerce/details.html',prod)
     
 def DescSortProd(request):
+    posts= Prods.objects.all().order_by('-fiyat')
+    page = request.GET.get('page')
+    num_of_item= 20
+    paginator= Paginator(posts,num_of_item)
     
-    prod={
-        "prods": Prods.objects.all().order_by('-fiyat').values
-    }
-    return render (request,'ecommerce/home.html',prod)
+    try:
+        posts=paginator.page(page)
+    except PageNotAnInteger:
+        page=1
+        posts=paginator.page(page)
+    except EmptyPage:
+        page=paginator.num_pages
+        posts=paginator.page(page)
+    
+    
+    
+    prod={'prods':posts,'paginator':paginator}
+    
+    return render(request,'ecommerce/home.html',prod)
+
 
 def AscSortProd(request):
+    posts= Prods.objects.all().order_by('fiyat')
+    page = request.GET.get('page')
+    num_of_item= 20
+    paginator= Paginator(posts,num_of_item)
     
-    prod={
-        
-        "prods":Prods.objects.all().order_by('fiyat').values
-    }
+    try:
+        posts=paginator.page(page)
+    except PageNotAnInteger:
+        page=1
+        posts=paginator.page(page)
+    except EmptyPage:
+        page=paginator.num_pages
+        posts=paginator.page(page)
+    
+    
+    
+    prod={'prods':posts,'paginator':paginator}
+    
     return render(request,'ecommerce/home.html',prod)
 
 def prodStarSort(request):
     
-    prod={
-        
-        "prods": Prods.objects.all().order_by('-puani').values
-    }
+    posts= Prods.objects.all().order_by('-puani')
+    page = request.GET.get('page')
+    num_of_item= 20
+    paginator= Paginator(posts,num_of_item)
+    
+    try:
+        posts=paginator.page(page)
+    except PageNotAnInteger:
+        page=1
+        posts=paginator.page(page)
+    except EmptyPage:
+        page=paginator.num_pages
+        posts=paginator.page(page)
+    
+    
+    
+    prod={'prods':posts,'paginator':paginator}
     
     return render(request,'ecommerce/home.html',prod)
+
+  
